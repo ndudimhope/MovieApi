ca25bb010f2da54642de46098851121e161aabc3
Hope Ndudim <hope.ndudim@thebulb.africa>
2021-07-25 08:03:21 +0100

    Add project files.

---
+++ Models/MovieDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi.Models
+{
+    public class MovieDbContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+        }
+    }
+}
